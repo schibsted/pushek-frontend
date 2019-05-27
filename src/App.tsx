51dd73ec9fa6749380961cac0830868b6d5fcf19
Damian Petla <damian.petla@gmail.com>
2019-05-27 21:29:53 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import pushek from './puszek.gif';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pushek} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is Pushek Frontend app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://schibsted.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Schibsted Devs
         </a>
       </header>
     </div>
