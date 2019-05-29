aacf507af15cc227e277eb65d88dc668e95ad1f0
Damian Petla <damian.petla@gmail.com>
2019-05-29 13:44:15 +0200

    Add firebase and material design dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import pushek from './puszek.gif';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import firebase from 'firebase';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={pushek} className="App-logo" alt="logo" />
-        <p>
-          This is Pushek Frontend app
-        </p>
-        <a
-          className="App-link"
-          href="http://schibsted.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Schibsted Devs
-        </a>
-      </header>
-    </div>
-  );
+
+firebase.initializeApp({
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN',
+  projectId: 'PROJECT_ID'
+});
+
+var db = firebase.firestore();
+
+class App extends React.Component {
+
+
+  loadDevices() {
+    db.collection("devices").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data().name}`);
+      });
+  });
+  }
+
+  render() {
+    return <List>
+      <ListItem button>
+        <ListItemText primary="Item 1"></ListItemText>
+      </ListItem>
+      <Button variant="contained" color="primary" onClick={this.loadDevices} >Hello Johannes!</Button>
+    </List>
+  }
 }
+
+// const App: React.FC = () => {
+//   return (
+//     <Button variant="contained" color="primary">
+//     Hello
+//       </Button>
+//   );
+// }
 
 export default App;
