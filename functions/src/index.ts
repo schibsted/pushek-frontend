873175bd09dc909fea4117c61d3dca407a5b8fb0
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-07-09 19:39:35 +0200

    Making the function only POSTable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+const app = express();
+app.post('/', (request, response) => {
   const original = request.query.text;
   response.send("Hello from Firebase! You've sent me: " + original);
-});
+})
+export const helloWorld = functions.https.onRequest(app);
