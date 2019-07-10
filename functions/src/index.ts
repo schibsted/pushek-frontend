729fb1e13f802c3c2f8ab69ffa994c000cac0540
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-07-10 09:56:08 +0200

    Changing the name of the exported path

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,6 @@ const getRndInteger = (min : number, max : number) => {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 const app = express();
@@ -13,4 +12,4 @@ app.post('/', (request, response) => {
   const r = getRndInteger(0, 9999).toString();
   response.send(r.padStart(4, '0'));
 })
-export const helloWorld = functions.https.onRequest(app);
+export const generatePin = functions.https.onRequest(app);
