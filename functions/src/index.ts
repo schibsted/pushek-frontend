d924284435e2f5dd9faf91022613f96153a1cdc4
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-07-10 09:55:24 +0200

    Simple randomized pin generation function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,16 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 
+const getRndInteger = (min : number, max : number) => {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 const app = express();
 app.post('/', (request, response) => {
-  const original = request.query.text;
-  response.send("Hello from Firebase! You've sent me: " + original);
+  const r = getRndInteger(0, 9999).toString();
+  response.send(r.padStart(4, '0'));
 })
 export const helloWorld = functions.https.onRequest(app);
