678ff8cf113f2cdef541aab24e7f3f2cbffbd0e4
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-07-10 12:25:33 +0200

    Saving generated pin in firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as express from 'express';
 
 const getRndInteger = (min : number, max : number) => {
@@ -8,8 +9,27 @@ const getRndInteger = (min : number, max : number) => {
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 const app = express();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const db = admin.firestore();
+
 app.post('/', (request, response) => {
-  const r = getRndInteger(0, 9999).toString();
-  response.send(r.padStart(4, '0'));
-})
+  const pin = getRndInteger(0, 9999).toString().padStart(4, '0');
+  const pinRef = db.collection('pins').doc(pin);
+  db.runTransaction((tx) =>
+    tx.get(pinRef)
+      .then(pinDoc => {
+        if (pinDoc.exists) {
+          throw new Error("Generated pin already exists");
+        }
+        tx.create(pinRef, {});
+        return pin;
+      }))
+      .then(p => response.send(p))
+      .catch(err => {
+        console.log(err, err.stack);
+        return response.status(500).send({ error: err }) });
+});
+
 export const generatePin = functions.https.onRequest(app);
