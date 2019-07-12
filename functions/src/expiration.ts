e280db1d00dfdd71b0fbf36bb38c9a1ea411632e
Jakub Janczak <jakub.janczak@schibsted.pl>
2019-07-12 11:50:41 +0200

    Renaming things for clarity

--- functions/src/expiration.ts
+++ functions/src/expiration.ts
@@ -2,31 +2,29 @@ import * as consts from './consts';
 
 export default (db : FirebaseFirestore.Firestore) => {
 
-  const findExpired = (now : number) : Promise<FirebaseFirestore.QuerySnapshot> => {
+  const findExpiredPins = (now : number) : Promise<FirebaseFirestore.QuerySnapshot> => {
     return db.collection('pins')
       .where(consts.EXPIRATION_TIMESTAMP_KEY, '>', now)
       .get();
   };
 
-  const expirePin = (pinRef : FirebaseFirestore.DocumentReference, batch : FirebaseFirestore.WriteBatch) : Promise<void> => {
-    console.log(`Expiring pin ${pinRef}`);
-    return db.collection('pins')
-      .doc(pinRef.id)
-      .collection('devices')
-      .get()
-      .then(devices => devices.forEach(device => batch.delete(device.ref)));
-  }
-
-  const deleteDocs = async (docs : FirebaseFirestore.QuerySnapshot) : Promise<void> => {
+  const expirePins = async (docs : FirebaseFirestore.QuerySnapshot) : Promise<void> => {
     if (docs.size > 0) {
       const batch = db.batch();
-      docs.forEach((doc) => expirePin(doc.ref, batch));
+      docs.forEach((doc) => {
+        console.log(`Expiring pin ${doc.ref}`);
+        return db.collection('pins')
+          .doc(doc.id)
+          .collection('devices')
+          .get()
+          .then(devices => devices.forEach(device => batch.delete(device.ref)));
+      });
       await batch.commit()
     }
   };
 
   return async () => {
-    const expired = await findExpired(new Date().getTime());
-    await deleteDocs(expired); 
+    const expired = await findExpiredPins(new Date().getTime());
+    await expirePins(expired); 
   }
 };
